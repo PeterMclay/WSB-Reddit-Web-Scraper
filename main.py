59278f4e459e60573072b0f30c4973a5d4a576d7
Peter McLay <36018782+PeterMclay@users.noreply.github.com>
2021-01-19 18:53:47 -0500

    cleaning up comment parsing

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import praw
 import keys
 import parse_csv
 import operator
-
+import time
 
 reddit = praw.Reddit(
     user_agent="Comment Extraction by u/pete_sauce",
@@ -27,17 +27,28 @@ def tckrcount(val):
 
 subreddit = reddit.subreddit("wallstreetbets")
 print(subreddit.title)
-i = 1
-for submission in subreddit.new(limit=100):
-    if i == 1:
-        i += 1
-        continue
-    if submission.created_utc < 1610780400:
+comments = 0
+subs = 0
+current_time = time.time()
+print('Current time '+ str(current_time))
+for submission in subreddit.new(limit=1000):
+    # if i == 1:
+    #     i += 1
+    #     continue
+    if submission.created_utc < (current_time - 86400):
         print('breaking out')
         break
-    submission.comments.replace_more(limit=None)
+    if submission.num_comments > 2000:
+        submission.comments.replace_more(limit=50)
+    else:
+        submission.comments.replace_more(limit=None)
+    subs += 1
     for comment in submission.comments.list():
         tckrcount(comment.body)
+        comments += 1
 
 sorted_stock_list = dict(sorted(dictionary.items(), key=operator.itemgetter(1),reverse=True))
+print('Comments parsed = '+str(comments))
+print('Submissions parsed = '+str(subs))
+sorted_stock_list = {key:val for key, val in sorted_stock_list.items() if val != 0}
 print(sorted_stock_list)
