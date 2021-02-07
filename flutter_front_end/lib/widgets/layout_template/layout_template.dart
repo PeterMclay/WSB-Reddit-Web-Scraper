16d9c0666dc48078b9144c4a2e864879b91f8460
Peter McLay <36018782+PeterMclay@users.noreply.github.com>
2021-02-06 21:03:41 -0500

    Changed the trash layout I was following earlier

--- flutter_front_end/lib/widgets/layout_template/layout_template.dart
+++ flutter_front_end/lib/widgets/layout_template/layout_template.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_front_end/widgets/stock_list.dart';
 import 'package:flutter_front_end/locator.dart';
 import 'package:flutter_front_end/routing/route_names.dart';
+import 'package:flutter_front_end/providers/app_provider.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter_front_end/routing/router.dart';
 import 'package:flutter_front_end/widgets/side_menu/side_menu.dart';
 import 'package:flutter_front_end/services/navigation_service.dart';
+import 'package:flutter_front_end/helpers/enumerators.dart';
 
-class LayoutTemplate extends StatelessWidget {
+class LayoutTemplate extends StatefulWidget {
+  @override
+  _LayoutTemplateState createState() => _LayoutTemplateState();
+}
+
+class _LayoutTemplateState extends State<LayoutTemplate> {
   final GlobalKey<ScaffoldState> _key = GlobalKey<ScaffoldState>();
+  final _tabs = [StockList(), StockList(), Text('hello')];
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,13 +26,40 @@ class LayoutTemplate extends StatelessWidget {
       key: _key,
       body: Row(
         children: [
-          SideMenu(),
+          NavigationRail(
+            selectedIndex: _selectedIndex,
+            labelType: NavigationRailLabelType.all,
+            onDestinationSelected: (int index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            destinations: [
+              NavigationRailDestination(
+                  icon: ImageIcon(
+                    AssetImage('images/wsb.png'),
+                  ),
+                  label: Text('r/wallstreetbets')),
+              NavigationRailDestination(
+                  icon: ImageIcon(
+                    AssetImage('images/stocks.png'),
+                  ),
+                  label: Text('r/stocks')),
+              NavigationRailDestination(
+                  icon: ImageIcon(
+                    AssetImage('images/stonks.png'),
+                  ),
+                  label: Text('r/investing')),
+            ],
+          ),
+          //SideMenu(),
           Expanded(
-            child: Navigator(
-              key: locator<NavigationService>().navigatorKey,
-              onGenerateRoute: generateRoute,
-              initialRoute: StockListRoute,
-            ),
+            child: _tabs[_selectedIndex],
+            // child: Navigator(
+            //   key: locator<NavigationService>().navigatorKey,
+            //   onGenerateRoute: generateRoute,
+            //   initialRoute: StockListRoute,
+            // ),
           ),
         ],
       ),
